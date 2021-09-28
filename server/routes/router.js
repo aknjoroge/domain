dc81fb943ddfad2895331499ec77c25fc057c616
alexander <alexkaranja629@gmail.com>
2021-09-28 01:59:36 -0700

    fixed routes

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,19 +1,9 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
-const services=require('../services/render');
+const services = require("../services/render");
 
-/**
- * @method <GET 
- * @description Home route
- */
-route.get('/',services.homeRoute);
+route.post("/verify", services.verifyDomain);
+
+module.exports = route;
 
 
-/**
- * @method <POST
- * @description Where the domain is verified for availabilty
- */
-route.post('/verify',services.verifyDomain);
-
-
-module.exports=route;
